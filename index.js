9c97db5589451397cbb2721ea60fcd42136cfbe5
Craig Clayton <mancityvsliverpoolfc@outlook.com>
2020-01-03 22:31:48 +0000

    passed Person.prototype challenge, index.js

--- index.js
+++ index.js
@@ -39,9 +39,20 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
+  Person.prototype.eat = function (someFood) {
+    if (this.stomach.length < 10) this.stomach.push(someFood);
+  };
 }
+Person.prototype.poop = function () {
+  this.stomach.length = 0;
+};
+Person.prototype.toString = function () {
+  return `${this.name} and ${this.age}`;
+};
 
 /*
   TASK 2
@@ -88,8 +99,16 @@ function Baby() {
 ///////// END OF CHALLENGE /////////
 if (typeof exports !== 'undefined') {
   module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Baby) { module.exports.Baby = Baby }
+  if (Airplane) {
+    module.exports.Airplane = Airplane
+  }
+  if (Person) {
+    module.exports.Person = Person
+  }
+  if (Car) {
+    module.exports.Car = Car
+  }
+  if (Baby) {
+    module.exports.Baby = Baby
+  }
 }
